d4765b513d66855606693d68449d0a5ed1cbec80
ISBAINE MOHAMED <isbainemouhamed@gmailcom>
2022-06-04 02:41:57 +0000

    cffv

---
+++ src/components/upload-files.component.js
@@ -0,0 +1,31 @@
+// components/simple-dropzone.component.js
+import React from "react";
+
+import Dropzone from 'react-dropzone-uploader'
+import 'react-dropzone-uploader/dist/styles.css'
+
+const SimpleDropZone = () => {
+
+    // Payload data and url to upload files
+    const getUploadParams = ({ meta }) => { return { url: 'http://localhost:5000' } }
+
+    // Return the current status of files being uploaded
+    const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
+
+    // Return array of uploaded files after submit button is clicked
+    const handleSubmit = (files, allFiles) => {
+        console.log(files.map(f => f.meta))
+        allFiles.forEach(f => f.remove())
+    }
+
+    return (
+        <Dropzone
+            getUploadParams={getUploadParams}
+            onChangeStatus={handleChangeStatus}
+            onSubmit={handleSubmit}
+            accept="image/*,audio/*,video/*"
+        />
+    );
+};
+
+export default SimpleDropZone;
